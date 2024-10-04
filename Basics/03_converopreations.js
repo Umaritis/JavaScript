8e26840b29423675ff99ff07920946a6b16165a1
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-04 21:23:23 +0000

    Convertions of data types

---
+++ Basics/03_converopreations.js
@@ -0,0 +1,24 @@
+let score =33
+// console.log(score)
+// console.log( typeof score)
+
+let changeScore = Boolean(score)
+// console.log(typeof score);
+// console.log(changeScore);
+
+
+let myname = "Muhammad Umar"
+// console.log(myname);
+
+// let changename = Number(myname)
+// console.log(changename);
+
+
+//Converstions are use to convert the one return type in to another type
+
+
+
+
+
+
+
