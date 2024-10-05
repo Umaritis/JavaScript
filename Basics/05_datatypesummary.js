bccb2ee6a5db08e41596474b78e2a4b5249795b0
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-05 08:18:02 +0000

    Data Types Summary

---
+++ Basics/05_datatypesummary.js
@@ -0,0 +1,33 @@
+//There are 2 types of datatypes in the js 
+
+
+//*************Premitive Datatypes************** */
+//String 
+// Boolean
+// Number
+// Symbol
+// BigInt
+// null
+// undefined
+
+//***************Non Premitive (Refrenced base types)********************* */
+
+// Arrays
+// Object
+// Funcations
+
+let heros =["Ahad", "Ahmad", "Muhammad"]
+console.table(heros);
+
+let myobj = {
+    fullName : "Muhammad Umar",
+    myAge : 19,
+    degree : "Bs Software egineering",
+}
+
+
+console.log(typeof myobj);
+
+
+// Every Datatype has some types 
+
