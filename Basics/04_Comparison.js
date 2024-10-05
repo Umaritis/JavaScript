99ac81c00608e09d700eed5073970744ac78e756
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-05 06:41:28 +0000

    Comparions Opretors

---
+++ Basics/04_Comparison.js
@@ -0,0 +1,14 @@
+//There are so many kinds of Comparison opreators
+
+console.log( 2 > 2);
+console.log(2 == 2);
+console.log(null== 0);
+console.log(undefined == 0);
+
+//You can use Comparison Opreator to compare the different values btut there are some draw backs 
+//in This language because when we compare an empty null valued type with zero its give the answer
+//false
+
+
+
+
