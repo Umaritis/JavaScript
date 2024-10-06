bbecca65e992a8f789d8520b74ba6f1a42c93f2a
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-06 20:23:34 +0000

    Arrays

---
+++ Basics_02/array.js
@@ -0,0 +1,39 @@
+//JavaScript arrays are resizable and can contain a mix of different data types.
+//JavaScript array-copy operations create shallow copies.
+
+let Arr = [1,2,3,4,5]
+
+// console.log(Arr);
+
+// console.log(Arr.includes(9));  
+// console.log(Arr.indexOf(4));
+// console.log(Arr.slice(2));
+
+
+// Arr.push(6)
+// Arr.pop()
+// Arr.unshift(10)
+// Arr.shift()
+
+console.log(Arr);
+
+// const newArr = Arr.join()  //This change the type of the array in to string 
+
+// console.log(newArr); 
+
+
+//*************Slice and Splice*************** */
+
+
+//Slice dont change the orignal array and it dont take the last value 
+
+//Splice change the orignal array 
+
+// console.log(Arr.slice(1,3));
+
+// Arr.splice(1,3)
+
+// console.log(Arr);
+
+
+
