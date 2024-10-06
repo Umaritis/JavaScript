03819b6f4a36da3779e96ebebd6c577059438ba2
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-06 19:45:09 +0000

    Dates in JavaScript

---
+++ Basics/07_dates.js
@@ -0,0 +1,34 @@
+// Dates in JS
+
+let myDate = new Date()   //Date is an object in JS
+
+//these are the funcation of printing the date in different form 
+
+// console.log(myDate);
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toLocaleString());
+
+
+let newDate = new Date(2024, 8, 20, 10, 15)
+
+// console.log(newDate.toLocaleString());
+
+
+let myCreatedDate = new Date ("2024, 09, 20")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = new Date()
+
+console.log(myTimeStamp.toLocaleString());
+
+con
+
+
+
