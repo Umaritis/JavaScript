d17cdfbd5e96f1fa4ec3c3050a2dadbdfa7fe66f
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-10 17:44:20 +0000

    Functions

--- Basics_03/01_functions.js
+++ Basics_03/01_functions.js
@@ -1,8 +1,19 @@
 function addTwoNumber(number1, number2) {
-    return number1+number2
+    // return number1+number2
 }
 
 const result =addTwoNumber(3,5);
-console.log(result);
+// console.log(result);
 
 //This is the basic way to to create and use the function 
+
+
+function userLoggedIn (username){
+    if(!username){
+        console.log("Please Logged In");
+        return
+    }
+    return `${username} just Logged IN`
+}
+
+console.log(userLoggedIn("Umar"))
