15f227059cecfb3cfb99ea11d71f43970ff17fd5
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-10 17:22:49 +0000

    Functions

---
+++ Basics_03/01_functions.js
@@ -0,0 +1,8 @@
+function addTwoNumber(number1, number2) {
+    return number1+number2
+}
+
+const result =addTwoNumber(3,5);
+console.log(result);
+
+//This is the basic way to to create and use the function 
