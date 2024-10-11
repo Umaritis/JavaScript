a970ffdf252dd700c332c770f5f1bb6e59f7b748
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-11 08:55:38 +0000

    Scop of Function

--- Basics_03/02_scop.js
+++ Basics_03/02_scop.js
@@ -4,4 +4,24 @@ function scop (){
     var c = 30;
 }
 
-console.log(c);
+// console.log(c);
+
+
+console.log(addone(10));
+
+function addone (num){
+    return num+1
+}
+
+
+
+console.log(12);
+
+//BY this way funcation will not executed because of the scop. Just beacuse the whole function is stored in the variable
+
+
+
+const addtwo = function(num){
+    return num +2
+}
+
