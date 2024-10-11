280c6aa2aa3514f3c0a328687673bd52374c0cef
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-11 19:37:31 +0000

    For Of loop

--- 05_loops/three.js
+++ 05_loops/three.js
@@ -3,28 +3,30 @@
 
 const arr = [1,2,3,4,5]
 for (const arree of arr) {
-    console.log(arree);   
+    // console.log(arree);   
 }
 
-const newArr = ["Superman", "spiderman", "batman"]
+// const newArr = ["Superman", "spiderman", "batman"]
 
-for (const man of newArr){
-    // console.log(man);
-}
+// for (const man of newArr){
+//     // console.log(man);
+// }
 
-const username = "Muhammad"
-for(const name of username){
-    // console.log(name)
-}
+// const username = "Muhammad"
+// for(const name of username){
+//     // console.log(name)
+// }
 
 
 const map = new Map()
-map.set('IS': "Islamabad")
-map.set('IS': "Islamabad")
-map.set('fr': "France")
-map.set('fr': "France")
-map.set('fr': "France")
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
 
-for (const key in map) {
-    console.log(map)
+// console.log(map);
+
+for (const [key, vlue] of map) {
+    console.log(key, ": " , vlue);
 }
+
