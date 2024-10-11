8c053ea04766685cb554e8d6b74990815057c48b
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-11 11:13:56 +0000

    IIFE

---
+++ Basics_03/04_iife.js
@@ -0,0 +1,15 @@
+//IIFE => Immediately Invoked Function Expression   (Nmaed IIFE)
+
+(function chai (name){
+    console.log(`MY name is ${name}`);
+    
+})("Muhammad Umar");
+
+
+//One more syntax of IIFE is following  UNname IIFE
+
+
+( (name) => {
+    console.log(`My name is ${name}`)
+    
+} )("Muhammad Umar")
