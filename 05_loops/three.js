57a606d6a7494b9a38ade3b4c120311f15565cfa
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-11 19:34:49 +0000

    Changes

---
+++ 05_loops/three.js
@@ -0,0 +1,30 @@
+//For of loop
+
+
+const arr = [1,2,3,4,5]
+for (const arree of arr) {
+    console.log(arree);   
+}
+
+const newArr = ["Superman", "spiderman", "batman"]
+
+for (const man of newArr){
+    // console.log(man);
+}
+
+const username = "Muhammad"
+for(const name of username){
+    // console.log(name)
+}
+
+
+const map = new Map()
+map.set('IS': "Islamabad")
+map.set('IS': "Islamabad")
+map.set('fr': "France")
+map.set('fr': "France")
+map.set('fr': "France")
+
+for (const key in map) {
+    console.log(map)
+}
