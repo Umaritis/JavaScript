6c5092dcad18e537e7d9201d46fa3f11e7a13d46
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-17 20:03:57 +0000

    For Each Loop

---
+++ 05_loops/five.js
@@ -0,0 +1,12 @@
+const coding = ["ruby", "python", "java", "cpp"]
+
+coding.push("css")
+// console.log(coding);
+
+
+//in the below for each loop we use the call back funcation becuasue the call back function dont has any name
+coding.forEach( function(languages){ //This is use to access the whole array no mattter what the size of the array is 
+    console.log(languages);
+} )
+
+
