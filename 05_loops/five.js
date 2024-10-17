a79ca9a71aa03cf1468df708d6f6f7b317955766
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-17 20:15:31 +0000

    Functions and For Each Loop

--- 05_loops/five.js
+++ 05_loops/five.js
@@ -6,7 +6,38 @@ coding.push("css")
 
 //in the below for each loop we use the call back funcation becuasue the call back function dont has any name
 coding.forEach( function(languages){ //This is use to access the whole array no mattter what the size of the array is 
-    console.log(languages);
+    // console.log(languages);
 } )
 
 
+coding.forEach( (lang) => {
+    // console.log(lang);
+    
+} )
+
+
+function printme (item){
+    console.log(item);
+}
+
+// coding.forEach(printme)
+
+
+const mycoding = [
+    {
+        langname: "Java",
+        langtitle: "Js",
+    },  {
+        langname: "c++",
+        langtitle: "c++",
+    },  {
+        langname: "python",
+        langtitle: "py",
+    },
+]
+
+
+mycoding.forEach( (lang2)=>{        //This is how we use For Each loop to get the data from the bjects of array 
+    console.log(lang2.langname);
+    
+} )
