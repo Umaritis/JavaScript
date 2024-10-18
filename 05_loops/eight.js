cd1f222f9d5e7ee60e1c7a26c0b5d2fcbb5ce4c8
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-18 20:51:24 +0000

    Reduce Example

--- 05_loops/eight.js
+++ 05_loops/eight.js
@@ -11,3 +11,28 @@ const newarr = arr.reduce(function (acc, currval) {
 
 
 
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
+
+
+
