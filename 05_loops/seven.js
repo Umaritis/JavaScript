a663992040abfa4eba47f6aab35fd78859252b11
Muhammad Umar <umarshahbazaas123@gmail.com>
2024-10-18 20:30:51 +0000

    Chaning Methods

---
+++ 05_loops/seven.js
@@ -0,0 +1,23 @@
+// const num = [1,2,3,4,5,6,7,8,9]
+
+// const newnum = num.map( (num) => num +10 )
+// console.log(newnum);
+
+// Map is same as the for each loop 
+
+// const newnumtwo = num.map( (nummm)=> nummm )
+
+// console.log(num);
+
+
+const newarr = [1,2,3,4,5,6,7,8,9]
+const newnum = newarr.map( (num) => num+2 )
+.map ( (num) => num*2 )
+
+console.log(newnum);
+
+//This is called the chaning in js in which we use multiple methods ofor the single array at the same time and the one method pass the update value to the next method 
+
+
+
+
